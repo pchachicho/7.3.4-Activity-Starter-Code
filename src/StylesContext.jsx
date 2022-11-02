83cb008ccff5e6f77d48174eaa183ebc6dc76257
Patrick hachicho <105758539+pchachicho@users.noreply.github.com>
2022-11-02 22:41:31 +0000

    Changed buttons with context

---
+++ src/StylesContext.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+const StylesContext = React.createContext({});
+
+export default StylesContext;
--- src/SubmitForm.jsx
+++ src/SubmitForm.jsx
@@ -1,7 +1,9 @@
 import React, { useContext } from "react";
 import RequestFormButton from './RequestFormButton'
+import StylesContext from './StylesContext.jsx'
 
 export default function SubmitForm() {
+  const buttonStyles = React.useContext(StylesContext)
   return (
     <div style={{display: "flex"}}>
       <form style={{display: "inline-block"}}>
